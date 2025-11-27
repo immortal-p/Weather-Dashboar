9ed2f39751d88de3dc627e47aa3a89fa5fcb1bd3
immortal-p <immortalkil4oy@gmail.com>
2025-11-27 11:37:17 +0300

    build: adding error handling for empty strings

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,33 +7,45 @@ interface SearchBarProprs {
 
 export function SearchBar({ onSearch, isLoading }: SearchBarProprs) {
     const [city, setCity] = useState('');
+    const [error, setError] = useState(false)
     
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
-        if(city.trim()) {
+        if (city.trim()) {
             onSearch(city);
+            setError(false)
+        }
+        else {
+            setError(true)
         }
         setCity('')
     };
 
     return (
-        <form onSubmit={handleSubmit} className="flex gap-2">
-            <input
-              id="log"
-              type="text"
-              value={city}
-              onFocus={(e) => e.target.select()}
-              onChange={e => setCity(e.target.value)}
-              placeholder="Enter ciry name..."
-              className="flex-1 text-neutral-300 p-2 rounded-lg border-2 transition duration-300 focus:outline-none focus:border-indigo-500"
-            />
-            <button
-              type="submit"
-              disabled={isLoading}
-              className="px-4 py-2 bg-indigo-500 duration-300 text-white rounded-lg hover:bg-indigo-600 disabled:bg-gray-400 disabled:cursos-not-allowed"
-            >
-                {isLoading ? 'Searching' : 'Search'}
-            </button>
-        </form>
+        <>
+          <form onSubmit={handleSubmit} className="flex gap-2 mb-2">
+              <input
+                id="log"
+                type="text"
+                value={city}
+                onFocus={(e) => e.target.select()}
+                onChange={e => setCity(e.target.value)}
+                placeholder="Enter ciry name..."
+                className="flex-1 text-neutral-300 p-2 rounded-lg border-2 transition duration-300 focus:outline-none focus:border-indigo-500"
+              />
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="px-4 py-2 bg-indigo-500 duration-300 text-white rounded-lg hover:bg-indigo-600 disabled:bg-gray-400 disabled:cursos-not-allowed"
+              >
+                  {isLoading ? 'Searching' : 'Search'}
+              </button>
+          </form>
+          {error && (
+            <div className="p-2 mb-2 text-red-400 animate-1 rounded-lg border-2">
+                <p>Error: Input field must not be empty</p>
+            </div>
+          )}
+        </>
     )
 }
